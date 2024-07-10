e47682bd090d5f9c334be864d7fbb15e0c311e7b
dohun1109 <dohun1109@naver.com>
2024-07-10 16:09:39 +0900

    Initial commit

---
+++ src/main/java/com/ync/hackathon/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.ync.hackathon;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationTest {
+
+    @GetMapping("/api/")
+    public String test() {
+        return "Hello World!!";
+    }
+
+    
+
+}
