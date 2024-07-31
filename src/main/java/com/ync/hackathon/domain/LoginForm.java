66acd096d55fda70c74f4b75874f58c8ce62b6a9
rohgibong <rohgibong@gmail.com>
2024-07-31 21:25:01 +0900

    로그인/로그아웃 기능 첫 구현

---
+++ src/main/java/com/ync/hackathon/domain/LoginForm.java
@@ -0,0 +1,18 @@
+package com.ync.hackathon.domain;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginForm {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 20)
+    private String pwd;
+}
