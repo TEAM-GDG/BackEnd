2d567383ad8affad8e09e79a3b6d9500b6cc241c
한상희 <tkdgml822@gmail.com>
2024-08-02 01:54:43 +0900

    스프링  bCryptPasswordEncoder 빈 추가

--- src/main/java/com/ync/hackathon/HackathonApplication.java
+++ src/main/java/com/ync/hackathon/HackathonApplication.java
@@ -2,10 +2,17 @@ package com.ync.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class HackathonApplication {
 
+	// 비밀번호 암호때 사용하는 bCryptPasswordEncoder 스프링 빈 등록
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HackathonApplication.class, args);
 	}
